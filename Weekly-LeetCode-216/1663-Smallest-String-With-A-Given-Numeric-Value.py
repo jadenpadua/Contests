1a839ed65a5355ef4c59a99850e3a7cbb9b4d178
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-11-25 20:51:55 -0800

    Create 1663-Smallest-String-With-A-Given-Numeric-Value.py

---
+++ Weekly-LeetCode-216/1663-Smallest-String-With-A-Given-Numeric-Value.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        res = ['a'] * n
+        k -= n
+
+        ht = defaultdict(str)
+        
+        for i, char in enumerate("abcdefghijklmnopqrstuvwxyz"):
+            ht[i] = char
+            
+        i = n - 1
+        
+        while k > 0:
+            temp = min(k, 25)
+            res[i] = ht[temp]
+            k -= temp
+            i -= 1
+        
+        return "".join(res)
